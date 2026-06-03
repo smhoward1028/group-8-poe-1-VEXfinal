3658684df9e24c0d7aa9d3db5c34daf690a226db
Shoshana Howard <smhoward1028@gmail.com>
2026-06-03 07:58:54 -0400

    Created new project

---
+++ g8-p1-VEXTransport/src/main.py
@@ -0,0 +1,19 @@
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# 	Module:       main.py                                                      #
+# 	Author:       shoshana                                                     #
+# 	Created:      6/3/2026, 7:58:10 AM                                         #
+# 	Description:  V5 project                                                   #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+# Library imports
+from vex import *
+
+# Brain should be defined by default
+brain=Brain()
+
+brain.screen.print("Hello V5")
+
+
+        
